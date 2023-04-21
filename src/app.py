2525f58968664fc4abaee005c4d49c1ef83c8951
brandon wilches <brandowilches2014@gmail.com>
2023-04-20 23:48:51 -0500

    ya casi termino

--- src/app.py
+++ src/app.py
@@ -21,7 +21,8 @@ from rutas.cita import routes_cita
 from rutas.implantes import routes_implantes
 from rutas.ortodoncias import routes_ortodoncias
 from rutas.registro import routes_registro
-from rutas.index import routes_index
+from rutas.index import routes_principal
+
 
 
 
@@ -32,7 +33,8 @@ app.register_blueprint(routes_cita, url_prefix="/fronted")
 app.register_blueprint(routes_implantes, url_prefix="/fronted")
 app.register_blueprint(routes_ortodoncias, url_prefix="/fronted")
 app.register_blueprint(routes_registro, url_prefix="/fronted")
-app.register_blueprint(routes_index, url_prefix="/fronted")
+app.register_blueprint(routes_principal, url_prefix="/fronted")
+
 
 
 
--- src/rutas/index.py
+++ src/rutas/index.py
@@ -2,10 +2,10 @@ from config.db import db, app, ma
 from flask import Blueprint, Flask,  redirect, request, jsonify, session, render_template
 from model.paciente import pacientes
 
-routes_index = Blueprint("routes_index", __name__)
+routes_principal = Blueprint("routes_principal", __name__)
 
 
-@routes_index.route("/indexindex" )
-def indexindex():
-    titulo= "Pagina index"
-    return render_template('/main/index.html', titles=titulo)
+@routes_principal.route("/indexprincipal" )
+def indexprincipal():
+    titulo= "Pagina principal"
+    return render_template('/main/principal.html', titles=titulo)
