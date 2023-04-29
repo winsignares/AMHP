090bbc756e6bfb330a1be1c4cb2a9ba8ee532dba
max <danielbanqueth024@gmail.com>
2023-04-29 14:53:13 -0500

    ss

---
+++ src/config/static/js/binj/mostrarcita_user.js
@@ -0,0 +1,35 @@
+function mostrar() {
+    const divcate = document.getElementById('tabla');
+    axios.get('mostrar_citas_user',{
+        responseType: 'json'
+    } )
+      
+      .then(function (response) {
+            let datos = response.data
+            var length = (Object.keys(datos).length) + 1;
+            let mostrar = '';
+            i= 0
+            for (let index = 1; index < length; index++) {
+                mostrar += ` <tr>   
+                <td>${datos[index].id}</td>  
+                <td>${datos[index].Nombre_completo}</td>
+                <td>${datos[index].Edad}</td>
+                <td>${datos[index].genero}</td>  
+                <td>${datos[index].fecha}</td>  
+                <td>${datos[index].consulta}</td>  
+                <td> <button onclick=" myFunction()">actualizar</button> 
+                
+
+              </tr> `;
+                
+            }
+            divcate.innerHTML = mostrar        
+      })
+      .catch(function (error) {
+        // Maneja los errores aquí
+        console.log(error);
+      });
+  }
+window.addEventListener('load', function() {
+    mostrar();
+})
--- src/rutas/cita2.py
+++ src/rutas/cita2.py
@@ -1,6 +1,6 @@
 from config.db import db, app, ma
 from flask import Blueprint, Flask,  redirect, request, jsonify, session, render_template
-from model.paciente import pacientes
+from model.cita import citas
 
 routes_cita2 = Blueprint("routes_cita2", __name__)
 
@@ -9,3 +9,24 @@ routes_cita2 = Blueprint("routes_cita2", __name__)
 def indexcita2():
     titulo= "Pagina cita2"
     return render_template('/main/cita2.html', titles=titulo)
+
+
+
+@routes_cita2.route('/mostrar_citas_user', methods=['GET'])
+def mostarcitasuser():
+    datos= {}
+    resultado = db.session.query(citas).select_from(citas).all()
+    i=0
+    goria = []
+    for cate in resultado:
+        i+=1	       
+        datos[i] = {
+        'id':cate.id,
+		'Nombre_completo':cate.Nombre_completo,
+		'Edad':cate.Edad,                                                    
+		'genero':cate.genero,                                                    
+		'fecha':cate.fecha,                                                    
+		'consulta':cate.consulta,                                                    
+        }
+        goria.append(datos)
+    return jsonify(datos)
