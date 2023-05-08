a7a0b451bd53ec797fb216a0a0f4adb50e5a461b
wilches018 <brandowilches2014@gmail.com>
2023-05-08 15:49:51 -0500

    modal de cistas disponibles

--- src/config/static/js/binj/funcion_modal.js
+++ src/config/static/js/binj/funcion_modal.js
@@ -1,25 +1,41 @@
 // Obtener el botón "Nueva cita"
 const botonNuevaCita = document.querySelector("#boton-nueva-cita");
+const botonNuevaCita2 = document.querySelector("#boton-nueva-cita-disponible");
+
 
 // Obtener el modal
 const modal = document.querySelector(".modal1");
+const modal2 = document.querySelector(".modal2");
+
 
 // Obtener el botón de cierre del modal
 const botonCerrar = document.querySelector(".close");
+const botonCerrar2 = document.querySelector(".close2");
 
 // Cuando se hace clic en el botón "Nueva cita", mostrar el modal
 botonNuevaCita.addEventListener("click", () => {
   modal.style.display = "block";
 });
-
+botonNuevaCita2.addEventListener("click", () => {
+    modal2.style.display = "block";
+  });
 // Cuando se hace clic en el botón de cierre del modal, ocultar el modal
 botonCerrar.addEventListener("click", () => {
   modal.style.display = "none";
 });
-
+botonCerrar2.addEventListener("click", () => {
+    modal2.style.display = "none";
+  });
 // Cuando el usuario hace clic fuera del modal, cerrar el modal
 window.addEventListener("click", (evento) => {
   if (evento.target == modal) {
     modal.style.display = "none";
   }
 });
+window.addEventListener("click", (evento) => {
+    if (evento.target == modal2) {
+      modal2.style.display = "none";
+    }
+  });
+  
+
