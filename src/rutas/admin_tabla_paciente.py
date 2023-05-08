4bf7480ae40ee0d0183c773233ce0172503f1c79
wilches018 <brandowilches2014@gmail.com>
2023-05-08 13:31:45 -0500

    aaa

---
+++ src/rutas/admin_tabla_paciente.py
@@ -0,0 +1,11 @@
+from config.db import db, app, ma
+from flask import Blueprint, Flask,  redirect, request, jsonify, session, render_template
+# from model.paciente import pacientes
+
+routes_principal = Blueprint("routes_principal", __name__)
+
+
+@routes_principal.route("/indexprincipal" )
+def indexprincipal():
+    titulo= "Pagina principal"
+    return render_template('/main/principal.html', titles=titulo)
