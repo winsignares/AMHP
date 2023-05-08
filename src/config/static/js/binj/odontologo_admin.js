73e917c906ecff572f51c3a7dbb156a755b5ae1f
max <danielbanqueth024@gmail.com>
2023-05-08 14:56:52 -0500

    se guarda y mestra con el modal de odontologo

---
+++ src/config/static/js/binj/odontologo_admin.js
@@ -0,0 +1,70 @@
+//esta funcion muestra los datos en una tabla inmediatamente que se habre la vista
+function mostrar() {
+    const divcate = document.getElementById('tabla');
+    axios.get('mostrar_odontologos_admin',{
+        responseType: 'json'
+    } )
+      
+      .then(function (response) {
+            let datos = response.data
+            var length = (Object.keys(datos).length) + 1;
+            let mostrar = '';
+            i= 0
+            for (let index = 1; index < length; index++) {
+                mostrar += ` <tr>   
+                <td>${datos[index].id}</td>  
+                <td>${datos[index].nombre}</td>
+                <td>${datos[index].direccion}</td>
+                <td>${datos[index].telefono}</td>  
+                <td>${datos[index].correo}</td>  
+                <td><a onclick="actualizar() "class="btn btn-primary btn-edit">Actualizar</a></td>
+                <td><a onclick="eliminar() " class="btn btn-danger btn-eliminar">Eliminar</a></td>
+              </tr> `;
+                
+            }
+            divcate.innerHTML = mostrar        
+      })
+      .catch(function (error) {
+        // Maneja los errores aquí
+        console.log(error);
+      });
+  }
+window.addEventListener('load', function() {
+    mostrar();
+})
+  
+//esta es la funcion de guardar paciente(registro) como admin utilizando la ruta de "admin_tabla_paciente.py"
+
+function registrar_odontologo() {
+    const Nombre = document.getElementById('Nombre');
+    const direccion = document.getElementById('direccion');
+    const Telefono = document.getElementById('Telefono');
+    const Correo = document.getElementById('Correo');
+    const Especialidad = document.getElementById('Especialidad');
+    alert('Registrar odontologo')
+
+    axios.post('guardarodontologos_admin', {
+        nombre: Nombre.value,
+        direccion: direccion.value,
+        telefono: Telefono.value,
+        correo: Correo.value,
+        especialidad: Especialidad.value,
+        
+
+    }, {
+        headers: { 
+        'Content-Type': 'multipart/form-data'
+
+        }
+    }
+    ).then((res) => {
+        console.log(res.data)
+        alert("registro existoso")
+        
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+
+    
+}
--- src/rutas/ortodoncias.py
+++ src/rutas/ortodoncias.py
@@ -1,6 +1,6 @@
 from config.db import db, app, ma
 from flask import Blueprint, Flask,  redirect, request, jsonify, session, render_template
-# from model.paciente import pacientes
+from model.odontologo import odontologos
 
 routes_ortodoncias = Blueprint("routes_ortodoncias", __name__)
 
@@ -8,4 +8,38 @@ routes_ortodoncias = Blueprint("routes_ortodoncias", __name__)
 @routes_ortodoncias.route("/indexortodoncias" )
 def indexortodoncias():
     titulo= "Pagina ortodoncias"
-    return render_template('/main/ortodoncias.html', titles=titulo)
+    return render_template('/main/ortodoncias.html', titles=titulo) 
+
+
+@routes_ortodoncias.route('/guardarodontologos_admin', methods=['POST'])
+def saveodontologos_admin():
+    nombre = request.form['nombre']
+    direccion = request.form['direccion']
+    telefono = request.form['telefono']
+    correo = request.form['correo']
+    especialidad = request.form['especialidad']
+    print(nombre)
+    new_reg = odontologos(nombre,direccion,telefono,correo,especialidad,)
+    db.session.add(new_reg)
+    db.session.commit()
+    return "si"
+
+
+@routes_ortodoncias.route('/mostrar_odontologos_admin', methods=['GET'])
+def mostarodontologo_admin():
+    datos= {}
+    resultado = db.session.query(odontologos).select_from(odontologos).all()
+    i=0
+    goria = []
+    for cate in resultado:
+        i+=1	       
+        datos[i] = {
+        'id':cate.id,
+		'nombre':cate.nombre,
+		'direccion':cate.direccion,                                                    
+		'telefono':cate.telefono,                                                    
+		'correo':cate.correo,                                                                                                       
+		'especialidad':cate.especialidad,                                                                                                       
+        }
+        goria.append(datos)
+    return jsonify(datos)
