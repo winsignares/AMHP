0bc6e6796f6add838db3cc4c00ea8cdbb88b0028
max <danielbanqueth024@gmail.com>
2023-05-18 13:39:47 -0500

    se crea el atributo en la tabla citas estado de citas

--- src/model/cita.py
+++ src/model/cita.py
@@ -13,11 +13,12 @@ class citas(db.Model):
     tarje_tade_credito = db.Column(db.String(50))
     Num_tarjeta = db.Column(db.String(50))
     problema = db.Column(db.String(250))
+    estado_citas = db.Column(db.String(250))
     id_paciente = db.Column(db.Integer,db.ForeignKey('tblpacientes.id'))
     id_odontologos = db.Column(db.Integer,db.ForeignKey('tblodontologos.id'))
     
 
-    def __init__(self, Nombre_completo, Edad,genero,fecha,consulta,tarje_tade_credito, Num_tarjeta,problema,id_paciente):
+    def __init__(self, Nombre_completo, Edad,genero,fecha,consulta,tarje_tade_credito, Num_tarjeta,problema,estado_citas):
         self.Nombre_completo = Nombre_completo
         self.Edad = Edad
         self.genero = genero
@@ -26,6 +27,7 @@ class citas(db.Model):
         self.tarje_tade_credito = tarje_tade_credito
         self.Num_tarjeta = Num_tarjeta
         self.problema = problema
+        self.estado_citas = estado_citas
     
 
 
